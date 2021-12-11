fc09c116531cd6544d04197f1b9241795b596897
Ali Azizi <aliazizi.79a@gmail.com>
2021-12-11 08:51:24 +0330

    Update SignInRemoteDatasource.kt

--- infrastructure/src/main/java/com/kotlinbyte/infrastructure/datasource/remote/SignInRemoteDatasource.kt
+++ infrastructure/src/main/java/com/kotlinbyte/infrastructure/datasource/remote/SignInRemoteDatasource.kt
@@ -1,14 +1,21 @@
 package com.kotlinbyte.infrastructure.datasource.remote
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.kotlinbyte.domain.vobject.AuthResult
 
 abstract class SignInRemoteDatasource {
     abstract suspend fun signIn(): AuthResult
+    abstract suspend fun signIn(googleSignInAccount: GoogleSignInAccount): AuthResult
+
 }
 
 class SignInRemoteDatasourceImpl : SignInRemoteDatasource() {
 
     override suspend fun signIn(): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun signIn(googleSignInAccount: GoogleSignInAccount): AuthResult {
         TODO("Not yet implemented")
     }
 
