1253e7553abaa6728a854dbb449792895a115866
Ali Azizi <aliazizi.79a@gmail.com>
2021-12-11 17:51:27 +0330

    Sign-Up view model created

--- presentation/src/main/java/com/kotlinbyte/presentation/features/signup/SignUpFragment.kt
+++ presentation/src/main/java/com/kotlinbyte/presentation/features/signup/SignUpFragment.kt
@@ -1,8 +1,20 @@
 package com.kotlinbyte.presentation.features.signup
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.kotlinbyte.domain_android_overlay.platform.BaseFragment
+import com.kotlinbyte.domain_android_overlay.platform.BaseViewBindingFragment
+import com.kotlinbyte.presentation.databinding.FragmentSignupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignUpFragment: BaseFragment() {
+class SignUpFragment :
+    BaseViewBindingFragment<FragmentSignupBinding>(FragmentSignupBinding::inflate) {
+
+    private val viewModel: SignUpViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
---
+++ presentation/src/main/java/com/kotlinbyte/presentation/features/signup/SignUpViewModel.kt
@@ -0,0 +1,21 @@
+package com.kotlinbyte.presentation.features.signup
+
+import androidx.lifecycle.ViewModel
+import com.kotlinbyte.domain.vobject.AuthResult
+import com.kotlinbyte.domain.vobject.Email
+import com.kotlinbyte.domain.vobject.Password
+import com.kotlinbyte.domain.vobject.Username
+import com.kotlinbyte.domain_android_overlay.platform.ReactiveViewModel
+import com.kotlinbyte.domain_android_overlay.usecase.SignUp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(private val signUpUseCase: SignUp) :
+    ReactiveViewModel<AuthResult>() {
+
+    fun signUp(username: Username, email: Email, password: Password) = asyncCallOnViewModelScope {
+        signUpUseCase.run(SignUp.Params(username, email, password))
+    }
+
+}
