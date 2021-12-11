0ca9967cdfe778678a87b94d15e4d650e584ac7d
Ali Azizi <aliazizi.79a@gmail.com>
2021-12-11 08:24:46 +0330

    Sign-In via google 🗝

--- presentation/src/main/java/com/kotlinbyte/presentation/features/signin/SignInFragment.kt
+++ presentation/src/main/java/com/kotlinbyte/presentation/features/signin/SignInFragment.kt
@@ -3,6 +3,7 @@ package com.kotlinbyte.presentation.features.signin
 import android.os.Bundle
 import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.fragment.app.viewModels
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.tasks.Task
 import com.kotlinbyte.domain_android_overlay.navigation.result.GoogleSignInResultContract
@@ -13,6 +14,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class SignInFragment : BaseFragment() {
 
+    private val viewModel: SignInViewModel by viewModels()
+
     @Inject
     lateinit var registry: ActivityResultRegistry
 
@@ -21,8 +24,10 @@ class SignInFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        registerForActivityResult(googleAuthSignInResultContract, registry) {
-
-        }
+        registerForActivityResult(
+            googleAuthSignInResultContract,
+            registry,
+            viewModel::signInViaGoogle
+        )
     }
 }
---
+++ presentation/src/main/java/com/kotlinbyte/presentation/features/signin/SignInViewModel.kt
@@ -0,0 +1,14 @@
+package com.kotlinbyte.presentation.features.signin
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.kotlinbyte.domain_android_overlay.platform.ReactiveViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class SignInViewModel : ReactiveViewModel<Nothing>() {
+
+    fun signInViaGoogle(task: Task<GoogleSignInAccount>?) {
+
+    }
+}
