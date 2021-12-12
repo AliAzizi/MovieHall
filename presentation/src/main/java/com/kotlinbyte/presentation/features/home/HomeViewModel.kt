66fdca077e913d1d1419f5092ec1678de2c0df0d
Ali Azizi <aliazizi.79a@gmail.com>
2021-12-12 07:02:45 +0330

    Home view model updated

--- presentation/src/main/java/com/kotlinbyte/presentation/features/home/HomeViewModel.kt
+++ presentation/src/main/java/com/kotlinbyte/presentation/features/home/HomeViewModel.kt
@@ -1,9 +1,10 @@
 package com.kotlinbyte.presentation.features.home
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(){
+class HomeViewModel @Inject constructor(): ViewModel() {
 
 }
